98fc70413a17333c797318c9c0d45698d96dc0ea
TomasCouso <couso4020@gmail.com>
2024-10-22 20:55:57 -0300

    Se ordeno el routes de home y se crearon mas archivos routes para que cada uno sea especifico de su entidad

--- routes/home.js
+++ routes/home.js
@@ -6,61 +6,14 @@ const { login } = require("../controllers/auth");
 
 const { getIndex } = require("../controllers/home");
 
-const {
-  getNoticias,
-  getNoticia,
-  createNoticia,
-  updateNoticia,
-  deleteNoticia,
-} = require("../controllers/noticias");
-
-const {
-  getInstructivos,
-  getInstructivo,
-  createInstructivo,
-  updateInstructivo,
-  deleteInstructivo,
-} = require("../controllers/instructivos");
-
-//TODO
-const {
-  getSolicitudes,
-  getSolicitud,
-  createSolicitud,
-  updateSolicitud,
-  deleteSolicitud,
-} = require("../controllers/solicitudes");
+router.use("/noticias", require("./noticias"));
+router.use("/instructivos", require("./instructivos"));
+router.use("/solicitudes", require("./solicitudes"));
 
 router.get("/", getIndex);
 
 //Seria login para poder hacer solicitudes
 router.get("/login", login);
-
-//Rutas para noticias
-router.get("/noticias", getNoticias);
-
-router.get("/noticias/:id", getNoticia);
-
-//estas 2 autorizadas y empleado o admin
-router.post("/noticias", createNoticia);
-
-router.put("/noticias/:id", updateNoticia);
-
-//esta autorizada y admin
-router.delete("/noticias/:id", deleteNoticia);
-
-// Rutas para instructivos
-router.get("/instructivos", getInstructivos);
-
-router.get("/instructivos/:id", getInstructivo);
-
-//estas 2 autorizadas y empleado o admin
-router.post("/instructivos", createInstructivo);
-
-router.put("/instructivos/:id", updateInstructivo);
-
-//esta autorizada y admin
-router.delete("/instructivos/:id", deleteInstructivo);
 
 //Rutas para solicitudes
 router.get("/solicitudes", getSolicitudes);
---
+++ routes/instructivos.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  getInstructivos,
+  getInstructivo,
+  createInstructivo,
+  updateInstructivo,
+  deleteInstructivo,
+} = require("../controllers/instructivos");
+
+router.get("/", getInstructivos);
+
+router.get("/:id", getInstructivo);
+
+//estas 2 autorizadas y empleado o admin
+router.post("/", createInstructivo);
+
+router.put("/:id", updateInstructivo);
+
+//esta autorizada y admin
+router.delete("/:id", deleteInstructivo);
